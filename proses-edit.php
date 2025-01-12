74600861876455dc0289c3702c1e9a4d81a28b63
MKhalfanRD <khalfanrd2002@gmail.com>
2025-01-12 23:06:34 +0700

    add proses-edit

--- proses-edit.php
+++ proses-edit.php
@@ -2,8 +2,24 @@
     include('config.php');
 
     if(isset($_POST['id'])){
-        $id = $_GET['id'];
+        $id = $_POST['id'];
+        $nama = $_POST['nama'];
+        $jk = $_POST['jenis_kelamin'];
+        $agama = $_POST['agama'];
+        $sekolah = $_POST['sekolah_asal'];
 
-        
+        $sql = "UPDATE calon_siswa SET nama='$nama', alamat='$alamat', jenis_kelamin='$jk', agama='$agama', sekolah_asal='$sekolah'";
+        $query = mysqli_query($db, $sql);
+
+        if($query)
+        {
+            header('Location: list-siswa.php');
+        } else {
+            die ('Gagal mengubah..');
+        }
+    }
+
+    else {
+        die ('Akses dilarang');
     }
 ?>
