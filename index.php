ce65ca607343e043594aea202c942d970f41ad31
MKhalfanRD <khalfanrd2002@gmail.com>
2025-01-12 12:23:06 +0700

    add index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pendaftaran Siswa</title>
+</head>
+<body>
+    <header>
+        <h3>Pendaftaran Siswa Baru</h3>
+        <h1>SMK Coding</h1>
+    </header>
+    <h4>menu</h4>
+    <nav>
+        <ul>
+            <li><a href="form-daftar.php">
+            <p>daftar baru</p>
+        </a></li>
+            <li><a href="list-siswa.php">
+            <p>pendaftar</p>
+        </a></li>
+        </ul>
+    </nav>
+</body>
+</html>
